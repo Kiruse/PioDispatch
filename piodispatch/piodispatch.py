46601aa3031c6653a26a9eebf08962c27b75a845
Kiruse <neurokiruse@outlook.com>
2021-01-07 13:03:12 +0100

    ascoroutine now properly wraps original function

--- piodispatch/piodispatch.py
+++ piodispatch/piodispatch.py
@@ -1,14 +1,15 @@
 """PioDispatch library's main module.
 Copyright (c) Kiruse 2020. See license in LICENSE."""
-import asyncio
 from asyncio.futures import wrap_future
 from concurrent.futures import ThreadPoolExecutor
 from typing import Awaitable, Callable, TypeVar
+import functools
 
 pool = ThreadPoolExecutor(100, 'io-runner-')
 T = TypeVar('T')
 
-def ascoroutine(fn: Callable) -> Callable:
+def ascoroutine(fn: Callable[..., T]) -> Callable[..., T]:
+    @functools.wraps(fn)
     async def inner(*args, **kwargs):
         return await dispatch(fn, *args, **kwargs)
     return inner
