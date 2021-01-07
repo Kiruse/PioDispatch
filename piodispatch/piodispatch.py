1150e87016c3008e200935b14dc55cc2d867b7a8
Kiruse <neurokiruse@outlook.com>
2021-01-07 12:56:52 +0100

    Improved type annotations
    `dispatch` now tells type linters that it returns the same type as the callback.

--- piodispatch/piodispatch.py
+++ piodispatch/piodispatch.py
@@ -3,16 +3,17 @@ Copyright (c) Kiruse 2020. See license in LICENSE."""
 import asyncio
 from asyncio.futures import wrap_future
 from concurrent.futures import ThreadPoolExecutor
-from typing import Callable
+from typing import Awaitable, Callable, TypeVar
 
 pool = ThreadPoolExecutor(100, 'io-runner-')
+T = TypeVar('T')
 
-def ascoroutine(fn: Callable):
+def ascoroutine(fn: Callable) -> Callable:
     async def inner(*args, **kwargs):
         return await dispatch(fn, *args, **kwargs)
     return inner
 
-def dispatch(callback: Callable, *args, **kwargs):
+def dispatch(callback: Callable[..., T], *args, **kwargs) -> Awaitable[T]:
     return wrap_future(pool.submit(callback, *args, **kwargs))
 
 def shutdown():
